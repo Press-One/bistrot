596f712872e85656c67a52ae365c3b5810c6fd9a
Leask Wong <i@leaskh.com>
2020-06-12 13:53:35 -0400

    update scripts

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 npm version patch \
 && npm upgrade utilitas \
-&& git commit -am 'npm upgrade utilitas' \
+&& ( git commit -am 'npm upgrade utilitas' || true ) \
 && git pull \
 && git push \
 && npm publish
