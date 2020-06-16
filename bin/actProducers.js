426c64f114a33a0f38af6e81a0160b7ca367465a
Leask Wong <i@leaskh.com>
2020-06-16 02:59:12 -0400

    adding SCALED_VOTES feature

--- bin/actProducers.js
+++ bin/actProducers.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const { producer } = require('sushitrain');
+const { finance, producer } = require('sushitrain');
+const mathjs = require('mathjs');
 
 const func = async (argv) => {
     const resp = await producer.getAll();
@@ -9,8 +10,12 @@ const func = async (argv) => {
             'TOTAL_PRODUCER_VOTE_WEIGHT:', resp.total_producer_vote_weight
         );
     }
+    const total = mathjs.bignumber(resp.total_producer_vote_weight);
     resp.rows.map(x => {
-        x.total_votes = x.total_votes.replace(/\.0*$/, '');
+        x.total_votes = x.total_votes.replace(/\.\d*$/, '');
+        x.scaled_votes = finance.bigFormat(
+            mathjs.divide(mathjs.bignumber(x.total_votes), total)
+        );
     });
     return resp.rows;
 };
@@ -27,11 +32,11 @@ module.exports = {
             columns: [
                 'owner',
                 'total_votes',
+                'scaled_votes',
                 'producer_key',
                 'is_active',
                 'unpaid_blocks',
                 'last_claim_time',
-                'location',
             ],
             config: {
                 singleLine: true,
@@ -40,7 +45,7 @@ module.exports = {
                     1: { alignment: 'right' },
                     2: { alignment: 'right' },
                     3: { alignment: 'right' },
-                    5: { alignment: 'right' },
+                    4: { alignment: 'right' },
                     6: { alignment: 'right' },
                     7: { alignment: 'right' },
                 },
