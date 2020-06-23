15214b6a3a13ecc1eeb448d98eebfcf888f5e95c
Leask Wong <i@leaskh.com>
2020-06-23 09:33:36 -0400

    upgrade balance command

--- bin/actBalance.js
+++ bin/actBalance.js
@@ -1,9 +1,27 @@
 'use strict';
 
-const { account } = require('sushitrain');
+const { account, finance } = require('sushitrain');
+const { utilitas } = require('utilitas');
 
 const func = async (argv) => {
-    return await account.getBalance(argv.account);
+    let r = [account.getBalance(argv.account), account.getByName(argv.account)];
+    const [bResp, aResp] = await Promise.all(r);
+    utilitas.assert(aResp, `Account not found: ${argv.account}`, 404);
+    const [currency, now] = [Object.keys(bResp)[0], new Date()];
+    const result = { 'balance': finance.bigFormat(bResp[currency], currency) };
+    if (aResp.refund_request) {
+        result.unstaking = finance.bigFormat(finance.bignumberSum(
+            finance.parseAmountAndCurrency(aResp.refund_request.net_amount)[0],
+            finance.parseAmountAndCurrency(aResp.refund_request.cpu_amount)[0],
+        ), currency);
+        const reqTime = new Date(`${aResp.refund_request.request_time}Z`);
+        const avlIn = (reqTime.getTime(
+        ) + 1000 * 60 * 60 * 24 * 3 - now.getTime()) / 1000 / 60 / 60;
+        result.unstak_available_in = `${Math.round(avlIn * 100) / 100} HR `;
+        result.unstak_request_time = reqTime.toUTCString();
+        result.timestamp = now.toUTCString();
+    }
+    return result;
 };
 
 module.exports = {
@@ -15,4 +33,14 @@ module.exports = {
         '    > Example:',
         '    $ prs-atm balance --account=ABCDE',
     ],
+    render: {
+        table: {
+            KeyValue: true,
+            config: {
+                columns: {
+                    0: { width: 20 }, 1: { width: 53, alignment: 'right' },
+                },
+            },
+        },
+    },
 };
