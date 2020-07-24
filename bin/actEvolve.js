49947d123af79f100980ef2b87358d3dc604586d
Leask Wong <i@leaskh.com>
2020-07-24 01:19:18 -0400

    adding beta feature for account and topic evolution

---
+++ bin/actEvolve.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const { chain } = require('../index');
+
+const func = async (argv) => {
+    const resp = await chain.accountEvolution(
+        argv.address, argv.prevkey, argv.account, argv.pubkey, argv.pvtkey
+    );
+    return resp;
+};
+
+module.exports = {
+    pubkey: true,
+    pvtkey: true,
+    func,
+    name: 'Evolve legacy PRESS.one accounts and Flying Pub topics',
+    help: [
+        '    --address  Legacy account, topic address     [STRING  / REQUIRED]',
+        '    --prevkey  Legacy account, topic private key [STRING  / REQUIRED]',
+        '    --account  PRESS.one account                 [STRING  / REQUIRED]',
+        '    --keystore Path to the keystore JSON file    [STRING  / OPTIONAL]',
+        '    --password Use to decrypt the keystore       [STRING  / OPTIONAL]',
+        '    --pubkey   PRESS.one public key              [STRING  / OPTIONAL]',
+        '    --pvtkey   PRESS.one private key             [STRING  / OPTIONAL]',
+        '    ┌---------------------------------------------------------------┐',
+        '    | 1. `keystore`(recommend) or `pubkey,pvtkey` must be provided. |',
+        '    └---------------------------------------------------------------┘',
+        '',
+        '    > Example:',
+        '    $ prs-atm evolve \\',
+        '              --address=0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ \\',
+        '              --prevkey=0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ \\',
+        '              --account=ABCDE \\',
+        '              --keystore=keystore.json',
+    ],
+};
