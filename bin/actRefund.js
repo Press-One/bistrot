fe4d72ba9df3f3f8912492d4e7a50780d9617eb2
Leask Wong <i@leaskh.com>
2020-07-27 17:41:55 -0400

    adding refund feature

---
+++ bin/actRefund.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const { atm } = require('../index');
+
+const func = async (argv) => {
+    return await atm.refund(argv.account, argv.pvtkey);
+};
+
+module.exports = {
+    pubkey: true,
+    pvtkey: true,
+    func,
+    name: 'Transfer the PRS in the refund to the balance',
+    help: [
+        '    --account  PRESS.one account                 [STRING  / REQUIRED]',
+        '    --keystore Path to the keystore JSON file    [STRING  / OPTIONAL]',
+        '    --password Use to decrypt the keystore       [STRING  / OPTIONAL]',
+        '    --pvtkey   PRESS.one private key             [STRING  / OPTIONAL]',
+        '    ┌---------------------------------------------------------------┐',
+        '    | 1. `keystore` (recommend) or `pvtkey` must be provided.       |',
+        '    | 2. Applicable when REFUND_AVAILABLE shows in balance output.  |',
+        '    └---------------------------------------------------------------┘',
+        '',
+        '    > Example:',
+        '    $ prs-atm refund --account=ABCDE --keystore=keystore.json',
+    ],
+};
