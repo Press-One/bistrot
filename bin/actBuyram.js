f587bba31e89d63d16eff1498fdc79f2917594f6
Leask Wong <i@leaskh.com>
2020-08-25 22:15:55 -0400

    finished defi daemon version beta

--- bin/actBuyram.js
+++ bin/actBuyram.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const { atm } = require('../index');
+const { account } = require('sushitrain');
 
 const func = async (argv) => {
-    return await atm.delegate(
-        argv.account, argv.receiver, argv.cpu, argv.net, argv.pvtkey
+    return await account.buyRam(
+        argv.account, argv.receiver, argv.ram, argv.pvtkey
     );
 };
 
@@ -12,26 +12,23 @@ module.exports = {
     pubkey: true,
     pvtkey: true,
     func,
-    name: 'Delegate CPU and/or Network Bandwidth',
+    name: 'Buy RAM',
     help: [
         '    --account  PRESS.one account                 [STRING  / REQUIRED]',
         "    --receiver Receiver's PRESS.one account      [STRING  / OPTIONAL]",
-        '    --cpu      PRS amount like xx.xxxx           [STRING  / OPTIONAL]',
-        '    --net      PRS amount like xx.xxxx           [STRING  / OPTIONAL]',
+        '    --ram      PRS amount like xx.xxxx           [STRING  / OPTIONAL]',
         '    --keystore Path to the keystore JSON file    [STRING  / OPTIONAL]',
         '    --password Use to decrypt the keystore       [STRING  / OPTIONAL]',
         '    --pvtkey   PRESS.one private key             [STRING  / OPTIONAL]',
         '    ┌---------------------------------------------------------------┐',
         "    | 1. Default `receiver` is current `account` (pvtkey holder).   |",
-        '    | 2. One of `cpu` or `net` must be provided.                    |',
         '    └---------------------------------------------------------------┘',
         '',
         '    > Example of delegating CPU and NET:',
-        '    $ prs-atm delegate \\',
+        '    $ prs-atm buyram \\',
         '              --account=ABCDE \\',
         '              --receiver=FIJKL \\',
-        '              --cpu=12.3456 \\',
-        '              --net=12.3456 \\',
+        '              --ram=12.3456 \\',
         '              --keystore=keystore.json',
     ],
 };
