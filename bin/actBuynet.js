0c3dfc5c10dba7548a4d45c50e61e21716e27fca
Leask Wong <i@leaskh.com>
2020-09-03 17:46:28 -0400

    archive buynet

--- bin/actBuynet.js
+++ bin/actBuynet.js
@@ -3,8 +3,8 @@
 const { account } = require('sushitrain');
 
 const func = async (argv) => {
-    return await account.buyRam(
-        argv.account, argv.receiver, argv.ram, argv.pvtkey
+    return await account.buyNet(
+        argv.account, argv.receiver, argv.net, argv.pvtkey
     );
 };
 
@@ -12,11 +12,11 @@ module.exports = {
     pubkey: true,
     pvtkey: true,
     func,
-    name: 'Buy RAM',
+    name: 'Buy NET',
     help: [
         '    --account  PRESS.one account                 [STRING  / REQUIRED]',
         "    --receiver Receiver's PRESS.one account      [STRING  / OPTIONAL]",
-        '    --ram      PRS amount like xx.xxxx           [STRING  / OPTIONAL]',
+        '    --net      PRS amount like xx.xxxx           [STRING  / OPTIONAL]',
         '    --keystore Path to the keystore JSON file    [STRING  / OPTIONAL]',
         '    --password Use to decrypt the keystore       [STRING  / OPTIONAL]',
         '    --pvtkey   PRESS.one private key             [STRING  / OPTIONAL]',
@@ -24,11 +24,11 @@ module.exports = {
         "    | 1. Default `receiver` is current `account` (pvtkey holder).   |",
         '    └---------------------------------------------------------------┘',
         '',
-        '    > Example of purchasing RAM:',
-        '    $ prs-atm buyram \\',
+        '    > Example of purchasing NET:',
+        '    $ prs-atm buynet \\',
         '              --account=ABCDE \\',
         '              --receiver=FIJKL \\',
-        '              --ram=12.3456 \\',
+        '              --net=12.3456 \\',
         '              --keystore=keystore.json',
     ],
 };
