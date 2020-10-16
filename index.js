2ae144e8d28fb225d530b6bf98968425afb7781c
Leask Wong <i@leaskh.com>
2020-10-16 01:41:04 -0400

    tweak index

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const fs = require('fs');
 const libPath = path.join(__dirname, 'lib');
 
 fs.readdirSync(libPath).filter((file) => {
-    return /\.js$/i.test(file) && file.toLowerCase() !== 'config.js';
+    return /\.js$/i.test(file)
+        && file.indexOf('.') !== 0
+        && file.toLowerCase() !== 'config.js';
 }).forEach((file) => {
     module.exports[file.replace(/^(.*)\.js$/, '$1')]
         = require(path.join(libPath, file));
