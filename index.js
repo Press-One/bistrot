60a506ca7c28ffa88f3909b4035c7a15eb9090a7
Leask Wong <i@leaskh.com>
2020-10-21 13:59:25 -0400

    remove adding progressive config demo

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 'use strict';
 
-const utilitas = require('utilitas');
 const path = require('path');
 const fs = require('fs');
 
-const lib = (options = {}) => {
-    utilitas.mergeAtoB(options, global.chainConfig = global.chainConfig || {});
-    return lib;
-};
-
-Object.assign(lib, utilitas);
+module.exports = require('utilitas');
 
 const libPath = path.join(__dirname, 'lib');
 fs.readdirSync(libPath).filter((file) => {
@@ -17,7 +11,6 @@ fs.readdirSync(libPath).filter((file) => {
         && file.indexOf('.') !== 0
         && file.toLowerCase() !== 'config.js';
 }).forEach((file) => {
-    lib[file.replace(/^(.*)\.js$/, '$1')] = require(path.join(libPath, file));
+    module.exports[file.replace(/^(.*)\.js$/, '$1')]
+        = require(path.join(libPath, file));
 });
-
-module.exports = lib;
