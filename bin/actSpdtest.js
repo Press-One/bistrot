0046314d8559106c93c52b8bfd83c6fe8ad94e1d
Leask Wong <i@leaskh.com>
2020-10-21 14:25:57 -0400

    update ss version

--- bin/actSpdtest.js
+++ bin/actSpdtest.js
@@ -1,12 +1,10 @@
 'use strict';
 
-global.chainConfig.speedTest = true;
-global.chainConfig.debug = true;
-
 const { sushitrain } = require('sushitrain');
 const { helper } = require('../');
 
 const func = async (argv) => {
+    Object.assign(global.chainConfig, { speedTest: true, debug: true });
     console.log('Evaluate RPC API nodes:');
     await sushitrain.getRpcUrl();
     console.log('\nEvaluate Chain API nodes:');
