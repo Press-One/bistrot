6d8d15a63409e7ccd0c423ee0f3ff827334ee7a9
Leask Wong <i@leaskh.com>
2020-11-21 16:04:16 -0500

    update dependencies

--- bin/actSwap.js
+++ bin/actSwap.js
@@ -5,24 +5,6 @@ global.chainConfig.rpcApi = 'http://51.255.133.170:8888';
 const { utilitas, swap, exchange } = require('..');
 
 const func = async (argv) => {
-    argv.from = utilitas.ensureString(argv.from, { case: 'UP' });
-    argv.to = utilitas.ensureString(argv.to, { case: 'UP' });
-    const [deal, pool] = [[argv.from, argv.to], await swap.getPool()];
-    utilitas.assert(argv.from, 'From-currency is required.', 400);
-    utilitas.assert(argv.to, 'From-currency is required.', 400);
-    utilitas.assert(pool && pool.length, 'No swap-pool is available.', 500);
-    let catchPool;
-    for (let x of pool) {
-        if (utilitas.arrayEqual(deal, x.tokens.map(y => { return y.symbol }))) {
-            catchPool = x;
-            break;
-        }
-    };
-    utilitas.assert(
-        catchPool,
-        `No pool is available to swap between ${argv.from} and ${argv.to}.`, 400
-    );
-    console.log(catchPool);
     const x = await exchange.swapToken(
         argv.pvtkey, argv.account, argv.receiver,
         argv['mx-id'], argv['mx-num'], argv.email, argv.from, argv.amount,
