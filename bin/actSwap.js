07eb9cedca377a1e57fa5b42b90e43a7561a8130
Leask Wong <i@leaskh.com>
2020-11-25 18:15:18 -0500

    update swapToken feature

--- bin/actSwap.js
+++ bin/actSwap.js
@@ -6,8 +6,8 @@ const { exchange } = require('..');
 
 const func = async (argv) => {
     const resp = await exchange.swapToken(
-        argv.pvtkey, argv.account, argv.receiver, argv.from, argv.amount,
-        argv.to, argv.slippage, argv.email,
+        argv.pvtkey, argv.account, null, // argv.receiver
+        argv.from, argv.amount, argv.to, argv.slippage, argv.email,
         argv['mx-id'], argv['mx-num'], argv.memo, { dryrun: argv.dryrun }
     );
     return resp;
