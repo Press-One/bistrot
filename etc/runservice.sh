36f47be7e7f44c739efb95813d0831a1edc80584
Leask Wong <i@leaskh.com>
2020-12-04 17:24:29 -0500

    update run service script

--- etc/runservice.sh
+++ etc/runservice.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-docker run --rm --name $1 -d -v /var/data/prschain:/opt/eosio/data-dir -p 8888:8888 -p 9876:9876 -p 8080:8080 -t dockerhub.qingcloud.com/pressone/chainsystem:latest nodeosd.sh $@
+docker pull pressone/chainsystem
+docker run --rm --name $1 -d \
+-v /var/data/prschain:/opt/eosio/data-dir \
+-p 8888:8888 -p 9876:9876 -p 8080:8080 \
+-t pressone/chainsystem nodeosstart.sh $@
