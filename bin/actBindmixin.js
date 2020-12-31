9a5a03e6c76d6c121a5770b7dc8a775e7233e776
Leask Wong <i@leaskh.com>
2020-12-31 00:28:32 -0500

    working on bindIdentity

--- bin/actBindmixin.js
+++ bin/actBindmixin.js
@@ -3,7 +3,25 @@
 const { atm } = require('..');
 
 const func = async (argv) => {
-    const resp = await atm.bindIdentity(argv.name);
+    const resp = await atm.bindIdentity(argv.account, argv.pvtkey);
+    // debug mode {
+    const { utilitas, mixin } = require('..');
+    const debug = utilitas.insensitiveCompare(
+        argv.rpcapi, 'http://51.255.133.170:8888'
+    );
+    if (debug) {
+        const uuid = '[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}';
+        const replace = {
+            recipient: '14da6c0c-0cbf-483c-987a-c44477dcad1b',
+            asset: mixin.assetIds.CNB.id,
+        };
+        for (let i in replace) {
+            resp.paymentUrl = resp.paymentUrl.replace(new RegExp(
+                `(^.*${i}=)${uuid}(&.*$)`
+            ), `$1${replace[i]}$2`);
+        }
+    }
+    // }
     if (!argv.json && resp && resp.paymentUrl) {
         console.log(`\nOpen this URL in your browser:\n\n${resp.paymentUrl}\n`);
     }
@@ -12,6 +30,7 @@ const func = async (argv) => {
 
 module.exports = {
     pubkey: true,
+    pvtkey: true,
     func,
     name: 'Open an Account',
     help: [
