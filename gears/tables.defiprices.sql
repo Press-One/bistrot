30c2d17bd7a35ad6808815035a027aa5f1928492
Leask Wong <i@leaskh.com>
2021-01-05 10:39:52 -0500

    removed defi support

--- gears/tables.defiprices.sql
+++ gears/tables.defiprices.sql
@@ -1,18 +1,19 @@
-\c sushitrain;
+-- This feature has been removed!
+-- \c sushitrain;
 
-create table defiprices (
-    id BIGSERIAL PRIMARY KEY,
-    currency VARCHAR NOT NULL,
-    base VARCHAR NOT NULL,
-    price VARCHAR NOT NULL,
-    accuracy BIGINT NOT NULL,
-    prices JSONB NOT NULL,
-    time TIMESTAMPTZ NOT NULL
-);
+-- create table defiprices (
+--     id BIGSERIAL PRIMARY KEY,
+--     currency VARCHAR NOT NULL,
+--     base VARCHAR NOT NULL,
+--     price VARCHAR NOT NULL,
+--     accuracy BIGINT NOT NULL,
+--     prices JSONB NOT NULL,
+--     time TIMESTAMPTZ NOT NULL
+-- );
 
-CREATE INDEX currency ON defiprices(currency);
-CREATE INDEX base     ON defiprices(base);
-CREATE INDEX price    ON defiprices(price);
-CREATE INDEX accuracy ON defiprices(accuracy);
-CREATE INDEX prices   ON defiprices(prices);
-CREATE INDEX time     ON defiprices(time);
+-- CREATE INDEX currency ON defiprices(currency);
+-- CREATE INDEX base     ON defiprices(base);
+-- CREATE INDEX price    ON defiprices(price);
+-- CREATE INDEX accuracy ON defiprices(accuracy);
+-- CREATE INDEX prices   ON defiprices(prices);
+-- CREATE INDEX time     ON defiprices(time);
