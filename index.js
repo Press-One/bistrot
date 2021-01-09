5bc70381a81b2f251efbe6eb702ab5062f26bfbe
Leask Wong <i@leaskh.com>
2021-01-09 02:29:48 -0500

    tweak building

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 'use strict';
 
-module.exports = Object.assign(require('sushitrain'), {
-    atm: require('./lib/atm'),
-    ballot: require('./lib/ballot'),
-    chain: require('./lib/chain'),
-    config: require('./lib/config'),
-    etc: require('./lib/etc'),
-    exchange: require('./lib/exchange'),
-    keychain: require('./lib/keychain'),
-    statement: require('./lib/statement'),
-    system: require('./lib/system'),
-    wallet: require('./lib/wallet'),
-});
+try {
+    module.exports = require('./dist');
+} catch (e) {
+    console.log('>>> Running in source mode.')
+    module.exports = require('./main');
+}
