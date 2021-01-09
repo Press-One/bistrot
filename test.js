d89721f960fecd43d71c408327a09ce48cc2f7d5
Leask Wong <i@leaskh.com>
2021-01-09 00:40:23 -0500

    webpack v1

--- test.js
+++ test.js
@@ -1,15 +1,7 @@
 
-// global.node = {
-//     TEXT_NODE: 3
-// };
-
-// const prsAtm = require('.');
 const prsAtm = require('./dist/prs-atm.output.js');
 
 (async () => {
-    console.log(await prsAtm.fetch.default('http://baidu.com'));
-    // console.log(prsAtm.shot.getCurrentPosition.toString());
-    // console.log(await prsAtm.shot.getCurrentPosition());
-    // const a = await prsAtm.producer.getAll();
-    // console.log(a);
+    const a = await prsAtm.producer.getAll();
+    console.log(a);
 })();
