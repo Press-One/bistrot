04ee42aabf7ca4ee425f0023588ab25ea2326eaf
Leask Wong <i@leaskh.com>
2021-01-09 00:27:45 -0500

    update dependencies

--- test.js
+++ test.js
@@ -3,10 +3,13 @@
 //     TEXT_NODE: 3
 // };
 
+// const prsAtm = require('.');
 const prsAtm = require('./dist/prs-atm.output.js');
 
 (async () => {
-    console.log(await prsAtm.network.getCurrentPosition());
+    console.log(await prsAtm.fetch.default('http://baidu.com'));
+    // console.log(prsAtm.shot.getCurrentPosition.toString());
+    // console.log(await prsAtm.shot.getCurrentPosition());
     // const a = await prsAtm.producer.getAll();
     // console.log(a);
 })();
