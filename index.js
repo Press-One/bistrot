943d127390aaf4bdff57518dce99250c1dc32997
Leask Wong <i@leaskh.com>
2021-01-11 18:54:15 -0500

    adding dryrun feature

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 'use strict';
 
 try {
-    module.exports = require('./dist');
-} catch (e) {
-    console.log('>>> Running in source mode.')
     module.exports = require('./main');
+    console.log('>>> Running in source mode.');
+} catch (e) {
+    if (/cannot find module.*main/i.test(e.message)) {
+        module.exports = require('./dist');
+    } else { console.log(e); }
 }
