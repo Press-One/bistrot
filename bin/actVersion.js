5428fcabb5fbf15fd08bab97ee45cee95f774c1a
Leask Wong <i@leaskh.com>
2021-01-12 18:09:42 -0500

    upgrade version command

--- bin/actVersion.js
+++ bin/actVersion.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { utilitas, sushitrain } = require('..');
+const { utilitas, sushitrain, shot } = require('..');
 const path = require('path');
 
 const exportSsConfig = [
@@ -17,22 +17,28 @@ const func = async (argv) => {
         package_version: `v${data.version}`,
         node_version: process.version,
         homepage: data.homepage,
-        repository: data.repository.url,
+        repository: data.repository ? data.repository.url : null,
         author: data.author,
         license: data.license,
     } : {};
     for (let p of ['sushitrain', 'utilitas']) {
-        data[`${p}_version`] = (await utilitas.which(
-            path.join(__filename, `../../node_modules/${p}/package.json`)
-        )).version;
+        try {
+            data[`${p}_version`] = (await utilitas.which(
+                path.join(__filename, `../../node_modules/${p}/package.json`)
+            )).version;
+        } catch (e) { }
     }
-    // try {
-    const ssConfig = await sushitrain.config();
-    exportSsConfig.map(x => {
-        data[x] = Array.isArray(ssConfig[x]) && !argv.json
-            ? ssConfig[x].join('\n') : ssConfig[x];
-    });
-    // } catch (e) { }
+    try {
+        const ssConfig = await sushitrain.config();
+        exportSsConfig.map(x => {
+            data[x] = Array.isArray(ssConfig[x]) && !argv.json
+                ? ssConfig[x].join('\n') : ssConfig[x];
+        });
+        data['sushitrain_inited'] = true;
+    } catch (e) { data['sushitrain_inited'] = false; }
+    try {
+        data['geolocation'] = await shot.getCurrentPosition();
+    } catch (e) { data['geolocation'] = null }
     return data;
 };
 
