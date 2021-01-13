98581cc1a38a01428e3ded6b0e280c3ca677d18c
Leask Wong <i@leaskh.com>
2021-01-13 00:53:10 -0500

    upgrade version check command

--- bin/actVersion.js
+++ bin/actVersion.js
@@ -3,10 +3,26 @@
 const { utilitas, sushitrain, shot, system } = require('..');
 const path = require('path');
 
-const exportSsConfig = [
+const [exportSsConfig, packed, failed, merged] = [[
     'debug', 'secret', 'speedTest', 'keosApi',
     'rpcApi', 'shpApi', 'chainApi', 'ipfsApi',
-];
+], 'PACKED', 'FAILED', '[MERGED]'];
+
+const verboseCheck = {
+    sushitrain_inited: async (argv, data) => {
+        const ssConfig = await sushitrain.config();
+        exportSsConfig.map(x => {
+            data[x] = Array.isArray(ssConfig[x]) && !argv.json
+                ? ssConfig[x].join('\n') : ssConfig[x];
+        });
+        return merged;
+    },
+    geolocation: shot.getCurrentPosition,
+    sushibar: system.chkCpVer,
+    latest_released_version: async () => {
+        return (await system.chkNwVer()).version;
+    },
+};
 
 const func = async (argv) => {
     const pkg = path.join(__dirname, '..', 'package.json');
@@ -26,29 +42,29 @@ const func = async (argv) => {
         try {
             data[`${p}_version`] = (await utilitas.which(
                 path.join(__dirname, `../node_modules/${p}/package.json`)
-            )).version || 'PACKED';
-        } catch (e) { data[`${p}_version`] = 'PACKED'; }
+            )).version || packed;
+        } catch (e) { data[`${p}_version`] = packed; }
     }
-    try {
-        const ssConfig = await sushitrain.config();
-        exportSsConfig.map(x => {
-            data[x] = Array.isArray(ssConfig[x]) && !argv.json
-                ? ssConfig[x].join('\n') : ssConfig[x];
-        });
-        data['sushitrain_inited'] = true;
-    } catch (e) { data['sushitrain_inited'] = false; }
-    try {
-        data['geolocation'] = await shot.getCurrentPosition();
-    } catch (e) { data['geolocation'] = null; }
-    try {
-        data['sushibar'] = await system.chkCpVer();
-    } catch (e) { data['sushibar'] = null; }
+    for (let i in argv.debug ? verboseCheck : {}) {
+        try {
+            const resp = await verboseCheck[i](argv, data);
+            if (resp !== merged) { data[i] = resp || failed; }
+        } catch (e) {
+            data[i] = `${failed}: ${e.message}`;
+        }
+    }
     return data;
 };
 
 module.exports = {
     func,
     name: 'List version info',
+    help: [
+        '    ┌---------------------------------------------------------------┐',
+        '    | 1. Please use option `--debug` to get verbose information.    |',
+        '    | 2. Please use option `--json` to get structured data.         |',
+        '    └---------------------------------------------------------------┘',
+    ],
     example: [
         {
             title: 'getting package version',
