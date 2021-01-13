99de2276b0b1ecedc25c87ca8e4813cb34a6f6d6
Leask Wong <i@leaskh.com>
2021-01-13 17:34:59 -0500

    working on test case

--- test.js
+++ test.js
@@ -5,18 +5,26 @@ const path = require('path');
 const fs = require('fs');
 const log = (content) => { return utilitas.modLog(content, __filename); };
 const split = () => { console.log(''); };
-const [testUser, results, errors] = ['testuser1', {}, []];
+const [testUser, prevAddress, prevKey, results, errors] = [
+    'testuser1', 'e5ad638ed1a4ec75c77488f6b3d4011fdc7782dc', '6c241da9a33408fb72464860e246ce40a1b05c0bbed8018f554aeeb4cb969d4d', {}, []
+];
 
 let successTest = 0;
 let failedTest = 0;
 let skippedTest = 0;
 
 const tests = {
+    'AccountOpen': {},
+
     'Account': { args: { name: testUser } },
     'AccountAuth': { args: { account: testUser } },
-    'AccountBind': {},
-    'AccountEvolve': {},
-    'AccountOpen': {},
+    'AccountBind': { args: { account: testUser } },
+    'AccountEvolve': { args: { account: testUser, address: prevAddress, prevkey: prevKey, } },
+
+
+
+
+
     'AssetBalance': {},
     'AssetCancel': {},
     'AssetDeposit': {},
@@ -62,7 +70,7 @@ const tests = {
 };
 
 const checkKeystore = async () => {
-    log('Environment Checking: Keystore...');
+    log('Checking keystore...');
     let ks;
     try {
         ks = (await storage.getConfig()).config.keystores[`${testUser}-owner`];
@@ -77,11 +85,16 @@ const checkKeystore = async () => {
 };
 
 const getAllCommands = async () => {
+    log('Initializing test case...');
     const resp = [];
     fs.readdirSync(path.join(__dirname, 'bin')).filter((file) => {
         return /\.js$/i.test(file) && file !== 'prs-atm.js';
     }).forEach((file) => {
-        resp.push(file.replace(/^act|\.js$/ig, ''));
+        const cmd = file.replace(/^act|\.js$/ig, '');
+        utilitas.assert(
+            tests[cmd], `Test case not found for command (cmd).`, 400
+        );
+        resp.push(cmd);
     });
     return resp;
 };
@@ -98,27 +111,26 @@ const formatArgs = (args) => {
 };
 
 const test = async (func) => {
-    const testConfig = tests[func] || {};
-    if (testConfig.skip) { skippedTest++; return; };
+    if (tests[func].skip) { skippedTest++; return; };
     let args = {};
     try {
-        args = (testConfig.pre ? await testConfig.pre() : null)
-            || testConfig.args || {};
+        args = (tests[func].pre ? await tests[func].pre() : null)
+            || tests[func].args || {};
     } catch (err) { log(err); }
     try {
         const argTxt = formatArgs(args);
         split();
         log(`>>> CASE ${successTest + failedTest + 1} `
             + `>>> \`$ prs-atm ${func} ${argTxt}\``);
-        results[func] = await (testConfig.overload ?
-            testConfig.overload(args)
+        results[func] = await (tests[func].overload ?
+            tests[func].overload(args)
             : shell.exec(`./bin/prs-atm.js ${func} ${argTxt}`));
         results[func] = results[func]
         // log(`Success: ${JSON.stringify(results[func], null, 2)}`);
-        log(`Success: ${JSON.stringify(results[func])}`);
+        log(`Success: ${results[func]}`);
         successTest++;
     } catch (err) {
-        errors.push(`Failed: ${err.message}`);
+        errors.push(err.message);
         log(errors[errors.length - 1]);
         failedTest++;
     }
@@ -128,8 +140,9 @@ const test = async (func) => {
     const start = process.hrtime();
     try {
         await checkKeystore();
+        await getAllCommands();
     } catch (e) { log(e.message); process.exit(1); }
-    for (let func of await getAllCommands()) { await test(func); }
+    for (let func in tests) { await test(func); }
     const duration = Math.round(process.hrtime(start)[1] / 1000000 / 10) / 100;
     split(); log(`Success: ${successTest}, Failed: ${failedTest}, `
         + `Skipped: ${skippedTest}, Time consuming: ${duration} seconds.`)
