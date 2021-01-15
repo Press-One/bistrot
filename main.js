f276fac320b68585600a15e3b1c26ba8cc6bef23
Leask Wong <i@leaskh.com>
2021-01-15 16:55:00 -0500

    upgrade magicPayment

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 global._prsAtm = Object.assign(require('./package.json'), {
     testNetRpcApi: 'http://51.255.133.170:8888',
     testNetChainApi: 'https://elm-sushibar.ngrok.io',
+    testNetOfficialMixin: '14da6c0c-0cbf-483c-987a-c44477dcad1b',
     authorityChainApi: 'https://prs-bp1.press.one',
 });
 
