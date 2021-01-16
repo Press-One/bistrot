b80f14474389490c8d550a7a95cf3b44871c4baa
Leask Wong <i@leaskh.com>
2021-01-15 22:49:00 -0500

    working on new claime feature

---
+++ bin/actRewardAuth.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const { atm } = require('..');
+
+const func = async (argv) => {
+    return await atm.authOfficialReward(argv.account, argv.pvtkey);
+};
+
+module.exports = {
+    pvtkey: true,
+    func,
+    name: 'Auth PRESS.one official node to claim rewards daily automatically',
+    help: [
+        '    --account  PRESS.one account                 [STRING  / REQUIRED]',
+        '    --keystore Path to the keystore JSON file    [STRING  / OPTIONAL]',
+        '    --password Use to decrypt the keystore       [STRING  / OPTIONAL]',
+        '    --pvtkey   PRESS.one private key             [STRING  / OPTIONAL]',
+        '    ┌---------------------------------------------------------------┐',
+        '    | 1. TODO TODO TODO TODO TODO                                   |',
+        '    └---------------------------------------------------------------┘',
+    ],
+    example: {
+        args: {
+            account: true,
+            keystore: true,
+        }
+    },
+};
---
+++ bin/actRewardUnauth.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const { atm } = require('..');
+
+const func = async (argv) => {
+    return await atm.unauthOfficialReward(argv.account, argv.pvtkey);
+};
+
+module.exports = {
+    pvtkey: true,
+    func,
+    name: 'Unauth PRESS.one official node of claiming rewards',
+    help: [
+        '    --account  PRESS.one account                 [STRING  / REQUIRED]',
+        '    --keystore Path to the keystore JSON file    [STRING  / OPTIONAL]',
+        '    --password Use to decrypt the keystore       [STRING  / OPTIONAL]',
+        '    --pvtkey   PRESS.one private key             [STRING  / OPTIONAL]',
+        '    ┌---------------------------------------------------------------┐',
+        '    | 1. TODO TODO TODO TODO TODO                                   |',
+        '    └---------------------------------------------------------------┘',
+    ],
+    example: {
+        args: {
+            account: true,
+            keystore: true,
+        }
+    },
+};
