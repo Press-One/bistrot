468d4625f72fc5f01dcf9cf3c339a726ddd52f5d
Leask Wong <i@leaskh.com>
2021-02-17 20:01:23 -0500

    upgrading building code

--- etc/build.js
+++ etc/build.js
@@ -5,18 +5,22 @@ const path = require('path');
 const fs = require('fs');
 
 const modLog = (content) => { return utilitas.modLog(content, 'BUILD ETC'); };
+const targetFile = 'index.json';
 const fileContent = {};
 
 modLog('Loading files...');
 (fs.readdirSync(__dirname) || []).filter(file => {
-    return file.indexOf('.') !== 0 && file !== __filename;
+    return file.indexOf('.') !== 0
+        && !new Set([path.basename(__filename), targetFile]).has(file);
 }).forEach(file => {
+    modLog(`> ${file}`);
     fileContent[file] = fs.readFileSync(path.join(__dirname, file), 'utf8');
 });
 
 modLog('Updating bundle...');
+modLog(`> ${targetFile}`);
 fs.writeFileSync(
-    path.join(__dirname, 'index.json'),
+    path.join(__dirname, targetFile),
     JSON.stringify(fileContent),
     { encoding: 'utf8', flag: 'w' }
 );
