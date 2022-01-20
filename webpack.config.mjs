54e8117b71b6b003885051b52f29f1dec3245f3c
Leask Wong <i@leaskh.com>
2022-01-20 18:49:04 -0500

    trying to build a pure esm module

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -1,3 +1,5 @@
+// @todo: https://github.com/webpack/webpack/issues/2933#issuecomment-774253975
+
 import { createRequire } from 'module';
 import { fileURLToPath } from 'url';
 import path from 'path';
@@ -36,9 +38,9 @@ const base = {
     },
     externals: [/cardinal/, { got: 'commonjs got' }],
     ignoreWarnings: [warning => {
-        return ((warning?.loc?.start?.line === 82 // utilitas.event
+        return ((warning?.loc?.start?.line === 84 // utilitas.event
             && warning?.loc?.start?.column === 26
-            && warning?.loc?.end?.line === 82
+            && warning?.loc?.end?.line === 84
             && warning?.loc?.end?.column === 57
         ));
     }],
@@ -47,44 +49,47 @@ const base = {
 
 export default [{
     ...base, ...{
-        target: ['node14', 'electron14-main'],
+        target: ['node16'], // , 'electron16-main'
+        experiments: { ...base.experiments, outputModule: true },
         output: {
             path: dist,
             filename: 'index.mjs',
             asyncChunks,
-            libraryTarget: 'umd',
+            library: { type: 'module' },
         },
     },
-}, {
-    ...base, ...{
-        target: ['web'],
-        output: {
-            path: dist,
-            filename: 'index.web.mjs',
-            asyncChunks,
-        },
-        plugins: [
-            new webpack.ProvidePlugin({ process: 'process/browser.js' }),
-            new NodePolyfillPlugin()
-        ],
-        resolve: {
-            ...base.resolve,
-            alias: {
-                ...base.resolve.alias,
-                child_process: false,
-                module: false,
-                ping: false,
-                solc: false,
-            },
-            fallback: {
-                fs: require.resolve('browserify-fs'),
-                web3: require.resolve('web3/dist/web3.min.js'),
-            },
-        },
-        externals: [
-            ...base.externals,
-            { 'node:buffer': '{}' },
-            { 'node:stream': '{}' },
-        ]
-    },
-}];
+},
+    // {
+    // ...base, ...{
+    //     target: ['web'],
+    //     output: {
+    //         path: dist,
+    //         filename: 'index.web.mjs',
+    //         asyncChunks,
+    //     },
+    //     plugins: [
+    //         new webpack.ProvidePlugin({ process: 'process/browser.js' }),
+    //         new NodePolyfillPlugin()
+    //     ],
+    //     resolve: {
+    //         ...base.resolve,
+    //         alias: {
+    //             ...base.resolve.alias,
+    //             child_process: false,
+    //             module: false,
+    //             ping: false,
+    //             solc: false,
+    //         },
+    //         fallback: {
+    //             fs: require.resolve('browserify-fs'),
+    //             web3: require.resolve('web3/dist/web3.min.js'),
+    //         },
+    //     },
+    //     externals: [
+    //         ...base.externals,
+    //         { 'node:buffer': '{}' },
+    //         { 'node:stream': '{}' },
+    //     ]
+    // },
+    // }
+];
