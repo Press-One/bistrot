e98c24ddc5c05f036aa95c0b3ba102c6e996fe8a
Leask Wong <i@leaskh.com>
2022-09-07 22:34:34 -0400

    adding blockScout support

--- index.mjs
+++ index.mjs
@@ -12,13 +12,13 @@ import * as yargs from 'yargs';
 import web3 from 'web3';
 // features
 import * as account from './lib/account.mjs';
+import * as blockScout from './lib/blockScout.mjs';
 import * as crypto from './lib/crypto.mjs';
 import * as erc20 from './lib/erc20.mjs';
 import * as erc721 from './lib/erc721.mjs';
 import * as etc from './lib/etc.mjs';
 import * as finance from './lib/finance.mjs';
 import * as mixin from './lib/mixin.mjs';
-import PACMAN from './lib/pacman.mjs';
 import * as paidGroup from './lib/paidGroup.mjs';
 import * as quorum from './lib/quorum.mjs';
 import * as rumsc from './lib/rumsc.mjs';
@@ -26,14 +26,15 @@ import * as sushibar from './lib/sushibar.mjs';
 import * as system from './lib/system.mjs';
 import config from './lib/config.mjs';
 import manifest from './lib/manifest.mjs';
+import PACMAN from './lib/pacman.mjs';
 
 const pacman = new PACMAN();
 const pacmvm = new PACMAN({ mvm: true });
 
 export * from 'utilitas';
 export {
-    abiDecoder, account, config, crypto, elliptic, erc20, erc721, etc,
-    ethereumUtil, finance, hdwalletProvider, keythereum, manifest, mixin,
+    abiDecoder, account, blockScout, config, crypto, elliptic, erc20, erc721,
+    etc, ethereumUtil, finance, hdwalletProvider, keythereum, manifest, mixin,
     pacman, pacmvm, paidGroup, quorum, rumsc, secp256k1, solc, sushibar, system,
     table, web3, yargs,
 };
@@ -46,8 +47,8 @@ globalThis._bistrot = {
 
 if (utilitas.utilitas.inBrowser() && !globalThis.bistrot) {
     globalThis.bistrot = {
-        ...utilitas, abiDecoder, account, config, crypto, elliptic, erc20,
-        erc721, etc, ethereumUtil, finance, hdwalletProvider, keythereum,
+        ...utilitas, abiDecoder, account, blockScout, config, crypto, elliptic,
+        erc20, erc721, etc, ethereumUtil, finance, hdwalletProvider, keythereum,
         manifest, mixin, pacman, paidGroup, quorum, rumsc, secp256k1, solc,
         sushibar, system, table, web3, yargs,
     };
