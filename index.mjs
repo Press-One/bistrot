7830e47d13df55251f7f3e1ef8bf697812f6260f
Leask Wong <i@leaskh.com>
2023-07-13 00:17:09 -0400

    tweak to match web3 v4

--- index.mjs
+++ index.mjs
@@ -39,9 +39,9 @@ export {
 };
 
 globalThis._bistrot = {
-    // testNetRpcApi: 'http://51.255.133.170:8888',
-    // testNetChainApi: 'https://elm-sushibar.ngrok.io',
-    testNetOfficialMixin: '14da6c0c-0cbf-483c-987a-c44477dcad1b',
+    testNetRpcApi: 'http://51.255.133.170:8888',           // nolonger available
+    testNetChainApi: 'https://elm-sushibar.ngrok.io',      // nolonger available
+    testNetOfficialMixin: '14da6c0c-0cbf-483c-987a-c44477dcad1b', // https://developers.mixin.one/apps/7000102337
 };
 
 if (utilitas.utilitas.inBrowser() && !globalThis.bistrot) {
